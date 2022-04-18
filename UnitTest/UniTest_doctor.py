411fcef3e69802169d16c7cee74c022add04ad52
Manoshiii <85054248+Manoshiii@users.noreply.github.com>
2022-04-18 23:02:32 +0600

    UnitTest folder uploaded
    This part is for unittesting.

---
+++ UnitTest/UniTest_doctor.py
@@ -0,0 +1,35 @@
+import unittest
+from test_doctors import Doctors
+
+class TestDoctors(unittest.Testcase):
+    def test_email(self):
+        doct_1 = Doctors('Faruq', 'Hasan', 500000)
+        doct_2 = Doctors('Hasib', 'Jaman', 600000)
+
+        self.assertEqual(doct_1.email, 'Faruq.Hasan@gmail.com')
+        self.assertEqual(doct_1.email, 'Hasib.Jaman@gmail.com')
+
+    def test_fullname(self):
+        doct_1 = Doctors('Faruq', 'Hasan', 500000)
+        doct_2 = Doctors('Hasib', 'Jaman', 600000)
+
+        self.assertEqual(doct_1.fullname, 'Faruq Hasan')
+        self.assertEqual(doct_2.fullname, 'Hasib Jaman')
+
+        doct_1 = 'John'
+        doct_2 = 'Joni'
+        
+        self.assertEqual(doct_1.fullname, 'Faruq Hasan')
+        self.assertEqual(doct_2.fullname, 'Hasib Jaman')
+    def test_apply_raise(self):
+
+        doct_1 = Doctors('Faruq', 'Hasan', 500000)
+        doct_2 = Doctors('Hasib', 'Jaman', 600000)
+
+        doct_1.apply_raise()
+        doct_2.apply_raise()
+
+        self.assertEqual(doct_1, 5289)
+        self.assertEqual(doct_2, 6289)
+if __name__ == '__main__':
+    unittest.main()
