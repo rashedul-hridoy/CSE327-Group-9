d871ff6cf9c6c509079e88732f8ade9646fd84db
fahim199785 <101120337+fahim199785@users.noreply.github.com>
2022-04-19 14:30:22 +0600

    Update tests_models.py

--- admin_panel/tests/tests_models.py
+++ admin_panel/tests/tests_models.py
@@ -5,7 +5,7 @@ from patient.models import Patient
 class TestModels(TestCase):
 
     def test_model_Doctor(self):
-        doctor_email=Doctor.objects.create(doctor_email="sahjalal.nilay@northsouth.edu")
+        doctor_email=Doctor.objects.create(doctor_email="fahim.nilay@northsouth.edu")
         doctor_password=Doctor.objects.create(doctor_password="1234")
         doctor_name=Doctor.objects.create(doctor_name="Shahruk Khan")
         doctor_gender=Doctor.objects.create(doctor_gender="Male")
@@ -15,7 +15,7 @@ class TestModels(TestCase):
 
 
     def test_model_patient(self):
-        patient_email=Patient.objects.create(patient_email="sahjalal1.nilay@northsouth.edu")
+        patient_email=Patient.objects.create(patient_email="faisal1.nilay@northsouth.edu")
         patient_password=Patient.objects.create(patient_password="1234")
         patient_name=Patient.objects.create(patient_name="Salman Khan")
         patient_gender=Patient.objects.create(patient_gender="Male") 
